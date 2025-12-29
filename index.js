a2bb4f87d1841603d516bc4c9b95ff20bf654df0
mdomorfaruk100 <mdomorfarukdeveloper988@gmail.com>
2025-12-29 22:37:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const {MongoClient, ServerApiVersion} = require('mongodb');
 const app = express();
 const port = 3000;
-const username = 'arabian';
-const password = 'jgj3j4j34io593459iorgfhgk';
-const uri = "mongodb+srv://arabian:jgj3j4j34io593459iorgfhgk@cluster0.2xwfxzc.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
     serverApi:{
